b83b177bdce1cd90c8e97237b7551b9e176d6c74
Afmarnob <afmarnob@gmail.com>
2026-08-05 13:38:40 +0600

    Fix API fallback URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ const config = {
   baseUrl:
     import.meta.env.VITE_API_BASE_URL ||
     import.meta.env.VITE_BASE_URL ||
-    "http://localhost:5000/api/v1",
+    "https://backend-ph-tour-management-system.vercel.app/api/v1",
 };
 
 export default config;
